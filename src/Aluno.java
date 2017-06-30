1840021fc0fb78bc989e0e06e2a25560d9171bd5
Byron <byronkamal@hotmail.com>
2017-06-30 00:15:34 -0300

    Inicio do projeto e inclusao da classe aluno

---
+++ src/Aluno.java
@@ -0,0 +1,31 @@
+
+public class Aluno {
+
+	private int matricula;
+	private String nome;
+	private String semestreIngresso;
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getNome () {
+		return nome;
+	}
+	
+	public void setNome (String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSemestreIngresso () {
+		return semestreIngresso;
+	}
+	
+	public void setSemestreIngresso(String semestreIngresso) {
+		this.semestreIngresso = semestreIngresso;
+	}
+}
